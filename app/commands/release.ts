35f728986f7c870bfc30812d21a165bb59236d18
◱ PixelyIon <pixelyion@protonmail.com>
2019-12-09 17:14:37 +0530

    Add tsc-watch and write branch to title for release

--- app/commands/release.ts
+++ app/commands/release.ts
@@ -6,7 +6,7 @@ export class Release extends Command {
     constructor() {
         super("release", "r", "The latest release from the most recent commit, you can specify the branch `(r {Branch})`")
     }
-    
+
     async run(msg: Message, args: string[]): Promise<void> {
         let commitUrl = new URL("https://api.github.com/repos/skyline-emu/skyline/commits")
         if (args[1])
@@ -18,10 +18,12 @@ export class Release extends Command {
             const suites = (await (await fetch(`https://api.github.com/repos/skyline-emu/skyline/commits/${commits[index]["sha"]}/check-suites`, { headers: { "Accept": "application/vnd.github.antiope-preview+json" } })).json())["check_suites"]
             for (const suite of suites) {
                 if (suite["status"] == "completed") {
-                    msg.channel.send(new RichEmbed({
-                        "title": "Latest Release",
-                        "description": `https://github.com/skyline-emu/skyline/commit/${commits[index]["sha"]}/checks?check_suite_id=${suite["id"]}`
-                    }))
+                    let embed = new RichEmbed({ "description": `https://github.com/skyline-emu/skyline/commit/${commits[index]["sha"]}/checks?check_suite_id=${suite["id"]}` })
+                    if (args[1])
+                        embed.setTitle(`Latest Release from ${args[1]}`)
+                    else
+                        embed.setTitle("Latest Release")
+                    msg.channel.send(embed)
                     return
                 }
             }
