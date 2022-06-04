b92edbe064d983731342ee1e9e77b7cc5c524453
PixelyIon <pixelyion@protonmail.com>
2022-06-04 21:21:26 +0530

    Minor fixes to the Game Status command

--- src/commands/gameStatus.ts
+++ src/commands/gameStatus.ts
@@ -1,5 +1,8 @@
-import { AccessLevel, Command, CommandError } from "./command";
+
 import { Message, MessageEmbed, MessageActionRow, MessageSelectMenu, MessageSelectOptionData } from "discord.js";
+import { AccessLevel, Command, CommandError } from "./command";
+import config from "../config.json";
+
 import { Octokit } from "@octokit/rest";
 const octokit = new Octokit();
 
@@ -11,27 +14,24 @@ export class GameStatus extends Command {
 
     async run(message: Message, content: string[]): Promise<void> {
         
-        if (content.length == 0) {
-            throw new CommandError("You must provide a game to lookup!");
-        }
+        if (content.length == 0)
+            throw new CommandError("You must provide the name of the game to search for");
 
         let gameName = content.join(" ");
         const queryString = gameName + "+repo:skyline-emu/skyline-games-list+type:issues+is:open";
-        let gitSearch = await octokit.request('GET /search/issues', { q:queryString });
+        let gitSearch = await octokit.request("GET /search/issues", { q:queryString });
         let gitSearchTitles = [];
 
-        if (gitSearch.data.items.length <= 0) {
+        if (gitSearch.data.items.length <= 0)
             throw new CommandError("No results found! Check for spelling errors, or manually test and add issue here: https://github.com/skyline-emu/skyline-games-list/issues");
-        }
 
-        for (const i of gitSearch.data.items) {
+        for (const i of gitSearch.data.items)
             gitSearchTitles.push(i.title);
-        }
 
         let embed = new MessageEmbed()
             .setColor("BLUE")
             .setTitle("**Skyline Game Compatibility Check**")
-            .setDescription("Please select your desired game from the following list:");
+            .setDescription(`<@${message.author.id}> Select your desired game from the list`);
         
         let rowOptions: MessageSelectOptionData[] = [];
         for (const i of gitSearchTitles) {
@@ -48,32 +48,22 @@ export class GameStatus extends Command {
         const row = new MessageActionRow()
             .addComponents(
                 new MessageSelectMenu()
-                    .setCustomId('selectionChoices')
-                    .setPlaceholder('Choose a game...')
+                    .setCustomId("selectionChoices")
+                    .setPlaceholder("Choose a game...")
                     .addOptions(rowOptions)
-            );
-        
-        const rowDisabled = new MessageActionRow()
-            .addComponents(
-                new MessageSelectMenu()
-                    .setCustomId('selectionChoices')
-                    .setPlaceholder('Choose a game...')
-                    .addOptions(rowOptions)
-                    .setDisabled(true)
             );
 
         let botMessage = await message.channel.send({ embeds: [embed], components: [row] });
         
-        const collector = botMessage.createMessageComponentCollector({ componentType: 'SELECT_MENU', time: 30000 });
+        const collector = botMessage.createMessageComponentCollector({ componentType: "SELECT_MENU", time: 30000 });
 
-        collector.on('collect', async i => {
-            let finalSearch = await octokit.request('GET /search/issues', { q:i.values[0] + "+repo:skyline-emu/skyline-games-list+type:issues+is:open" });
+        collector.on("collect", async i => {
+            let finalSearch = await octokit.request("GET /search/issues", { q:i.values[0] + "+repo:skyline-emu/skyline-games-list+type:issues+is:open" });
             if (i.user.id === message.author.id) {
-                botMessage.edit({ components: [rowDisabled]});
                 let labelNames = "";
-                for (const i of finalSearch.data.items[0].labels){
-                    labelNames += `(${i.name}) `
-                }
+                for (const i of finalSearch.data.items[0].labels)
+                    labelNames += `\`${i.name}\` `;
+                
                 let body = finalSearch.data.items[0].body!.split("###");
                 body.splice(0, 2);
                 body.splice(5, 1);
@@ -81,14 +71,17 @@ export class GameStatus extends Command {
                 body[0] = body[0].replace("Tested Build", "");
                 body[1] = body[1].replace("Android Version", "Android ");
                 body[2] = body[2].replace("SoC type", "using ");
+                
                 let deviceDetails = [];
                 deviceDetails.push(body[1], body[2]);
+                
                 let build = body[0].replace(/\n|\r/gm, "");
                 deviceDetails[1] += " Processor";
                 body.splice(0, 3);
+                
                 let logs = body[1].replace("Log file", "");
-                if (logs.indexOf("```") == -1){
-                    i.reply({
+                if (logs.indexOf("```") == -1) {
+                    botMessage.edit({
                         embeds: [new MessageEmbed()
                             .setColor("BLUE")
                             .setTitle(finalSearch.data.items[0].title)
@@ -98,11 +91,12 @@ export class GameStatus extends Command {
                             .addField("Build", build)
                             .addField("Game Behavior", body[0].replace("Game Behaviour", "").replace(/!/gm, "").replace("- ", ""))
                             .addField("Logs", logs)
-                            .setFooter({ text: `Issue #${finalSearch.data.items[0].number}`, iconURL: 'https://avatars.githubusercontent.com/u/52578041' })
-                        ]
+                            .setFooter({ text: `Issue #${finalSearch.data.items[0].number}`, iconURL: "https://avatars.githubusercontent.com/u/52578041" })
+                        ],
+                        components: []
                     });
                 } else {
-                    i.reply({
+                    botMessage.edit({
                         embeds: [new MessageEmbed()
                             .setColor("BLUE")
                             .setTitle(finalSearch.data.items[0].title)
@@ -112,25 +106,27 @@ export class GameStatus extends Command {
                             .addField("Build", build)
                             .addField("Game Behavior", body[0].replace("Game Behaviour", "").replace(/!/gm, "").replace("- ", ""))
                             .addField("Logs", logs.substring(0, logs.indexOf("```")))
-                            .setFooter({ text: `Issue #${finalSearch.data.items[0].number}`, iconURL: 'https://avatars.githubusercontent.com/u/52578041' })
-                        ]
+                            .setFooter({ text: `Issue #${finalSearch.data.items[0].number}`, iconURL: "https://avatars.githubusercontent.com/u/52578041" })
+                        ],
+                        components: []
                     });
                 }
-            } else {
-                i.reply("bad");
             }
         });
 
-        collector.on('end', collected => {
+        collector.on("end", collected => {
             if (collected.size == 0) {
-                botMessage.edit({ components: [rowDisabled]});
-                botMessage.reply({
+                botMessage.edit({
                     embeds: [new MessageEmbed()
                         .setTitle("Error")
                         .setDescription("No game selected within 30 seconds! Please try again.")
                         .setColor("RED")
-                    ]
+                    ],
+                    components: []
                 });
+
+                if (botMessage instanceof Message)
+                    setTimeout(() => botMessage.delete(), config.deleteTime);
             }
         });
                  
