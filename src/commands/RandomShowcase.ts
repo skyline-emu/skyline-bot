c5958dc3425f889c9b1ac83175992f6577ab174b
Silverdragon122 <101513517+Silverdragon122@users.noreply.github.com>
2022-10-27 12:27:53 -0400

    Switch to .json
    After a ton of time I managed to get it to store .json upon a new message which stores it in 1 line in the showcase.json file. Also updated RandomShowcase.ts to support showcase.json

--- src/commands/RandomShowcase.ts
+++ src/commands/RandomShowcase.ts
@@ -6,11 +6,23 @@ export class RandomShowcase extends Command {
         super("randomshowcase", "rs", "Random message from showcase attachment database.", AccessLevel.User);
     }
     async run(message: Message, args: Array<string>): Promise<void> {
-        //Read the showcase.txt contents, split it into an array, and get a random element from the array
-        const showcasecontext = fs.readFileSync("showcase.txt", "utf8");
-        const array = showcasecontext.split("\n");
-        const randomElement = array[Math.floor(Math.random() * array.length)];
-        //Send the random element to the channel
-        await message.channel.send(randomElement);
-}
+        //Read the showcase.json file and choose a message. (each message is in between {}) Then:
+        //1. Get all the {} brackets
+        //2. Choose a random one
+        //3. Send a message without the brackets. Here is an example:{"message":"https://www.youtube.com/watch?v=CMtF7nxMv5A","author":"Silverdragon_12"}
+        //4. In that example, the message would be: https://www.youtube.com/watch?v=CMtF7nxMv5A Showcase Message By: Silverdragon_12
+        let showcase = fs.readFileSync("showcase.json", "utf8");
+        let showcasearray = showcase.split("},");
+        let randomshowcase = showcasearray[Math.floor(Math.random() * showcasearray.length)];
+        randomshowcase = randomshowcase.replace("{", "");
+        randomshowcase = randomshowcase.replace("}", "");
+        let randomshowcasearray = randomshowcase.split(",");
+        let randomshowcasemessage = randomshowcasearray[0];
+        let randomshowcaseauthor = randomshowcasearray[1];
+        randomshowcasemessage = randomshowcasemessage.replace('"message":"', "");
+        randomshowcasemessage = randomshowcasemessage.replace('"', "");
+        randomshowcaseauthor = randomshowcaseauthor.replace('"author":"', "");
+        randomshowcaseauthor = randomshowcaseauthor.replace('"', "");
+        message.channel.send(randomshowcasemessage + " Showcase Message By: " + randomshowcaseauthor);
+    }
 }
--- src/filters/OnShowcaseMsg.ts
+++ src/filters/OnShowcaseMsg.ts
@@ -11,17 +11,27 @@ export class OnShowcaseMsg extends Filter {
     //3. If it is a link, add it to the showcase.json file
     async run(message: Message<boolean>): Promise<boolean> {
         if (message.channel.id == "986020197146169354") {
-            if (message.content.startsWith("https://")) {
-                //Read the showcase.txt contents
-                const showcasecontext = fs.readFileSync("showcase.txt", "utf8");
-                const array = showcasecontext.split("\n");
-                array.push(message.content);
-                let newFileContent = array.join("\n");
-                fs.writeFileSync("showcase.txt", newFileContent);
-                console.log("Showcase attachment added!");
+            if (message.content.includes("https://")) {
+                let msginfo = {
+                    "message": message.content,
+                    "author": message.author.username,
+                }
+                //Find the [] brackets in the showcase.json file
+                let showcase = fs.readFileSync("showcase.json", "utf8");
+                //If the file is empty, do not use a comma
+                showcase = showcase.replace("[", "");
+                showcase = showcase.replace("]", "");
+                showcase = showcase + "," + JSON.stringify(msginfo);
+                //Add the brackets back to the showcase.json file
+                showcase = "[" + showcase + "]";
+                //If the file has nothing other than the brackets, remove the comma
+                showcase = showcase.replace("[,", "[");
+                showcase.replace(",]", "]");
+                fs.writeFileSync("showcase.json", showcase);
+                console.log("Succesfully added "+ message.author.username + "'s message to showcase.json");
             }
         }
-        return true;
-    }
 
-}
+    return true;
+        }
+    }
