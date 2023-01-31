5db35f0ccf69a499bea2acd47e2146fc534ef53d
finroy <gt9322@gmail.com>
2023-01-31 18:22:08 -0500

    Remove obsolete files from old bot system

--- src/common/roleAssignment.ts
+++
@@ -1,6 +0,0 @@
-/** This defines the format of a role assignment block in the configuration */
-interface RoleAssignment {
-    messageId: string;
-    emojiId: string;
-    roleId: string;
-}
--- src/filters/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./commandFilter";
-export * from "./log";
-export * from "./attachmentOnly";
