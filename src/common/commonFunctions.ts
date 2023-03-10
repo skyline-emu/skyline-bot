a23aeadcfdc0a8b5dcacc7e73e8833ae52ca3207
FinRoy <gt9322@gmail.com>
2023-03-09 19:56:30 -0500

    Fix minor formatting error + add isSnowflake check

--- src/common/commonFunctions.ts
+++ src/common/commonFunctions.ts
@@ -11,6 +11,14 @@ export enum AccessLevel {
 }
 
 /** 
+ * @param string The string to test
+ * @return If the tested string is a snowflake or not
+ */
+export function isSnowflake(testString: string) {
+    return (/^\d{15,20}$/.test(testString));
+}
+
+/** 
  * @param level The AccessLevel to lookup the role of
  * @return The Snowflake that corresponds to the role of the supplied AccessLevel
  */
@@ -36,7 +44,7 @@ export function getAccessLevelRole(level: AccessLevel): Snowflake {
  * @param level The access level to check if the user has
  * @returns If the supplied user has the specified access level in the specified guild 
  */
-export function userHasAccess(user: User, guild: Guild, level: AccessLevel){
+export function userHasAccess(user: User, guild: Guild, level: AccessLevel) {
     if (level == AccessLevel.User)
         return true;
 
